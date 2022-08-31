a3a98d407562646993621922a80fab7dc956d4a0
Dastan94 <dastan.abdrasulov@gmail.com>
2022-08-31 14:30:20 +0600

    Initial commit

---
+++ src/com/company/Car.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.io.IOException;
+import java.sql.SQLOutput;
+
+public class Car implements AutoCloseable{
+
+    public void drive() {
+        System.out.println("Car is going");
+    }
+    @Override
+    public  void close(){
+        System.out.println("Car is closing");
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        try (Car car = new Car()) {
+            car.drive();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+}
+
+
+
